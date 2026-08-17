32b3463ee864270ee2be28dc5818a01f9d2cd073
Piyush Jadhav <piyushjad04@gmail.com>
2026-08-16 20:27:13 -0700

    added pydantic to backend for data validation

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
 import string
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
-tasks_list = {0 : 'homework'}
+tasks_list = {0: {"id": 0, "title": "homework", "done": False}}
 num = 1
+
+
+class CreateTask(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
+    done: bool = False
+
+class UpdateTask(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
+    done: bool = False
+
+class TaskDone(BaseModel):
+    id: int 
+    title: str
+    done: bool
 
 
 @app.get("/")
@@ -17,11 +32,12 @@ def get_all_tasks():
     return tasks_list 
 
 @app.post("/tasks")
-def create_task(task: str):
+def create_task(task: CreateTask):
     global num
-    tasks_list[num] = task
+    new_task = {"id": num, "title": task.title, "done": task.done}
+    tasks_list[num] = new_task
     num += 1
-    return {"message": "Task Created"}
+    return new_task
 
 @app.get("/tasks/{id}")
 def get_specific_task(id: int):
@@ -30,11 +46,12 @@ def get_specific_task(id: int):
     return tasks_list[id]
 
 @app.put("/tasks/{id}")
-def edit_task(id: int, message: str):
+def edit_task(id: int, task: UpdateTask):
     if id not in tasks_list:
         raise HTTPException(status_code=404, detail="Item not found")
-    tasks_list[id] = message 
-    return {"message": "Edited"}
+    updated_task = {'id': id, 'title': task.title, 'done': task.done}
+    tasks_list[id] = updated_task
+    return updated_task
 
 @app.delete("/tasks/{id}")
 def delete_task(id: int):
