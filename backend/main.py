45947b83b485d487ec63f9a6055f923a499b7a41
Piyush Jadhav <piyushjad04@gmail.com>
2026-08-27 20:58:26 -0700

    added a new table in databse for projects, made foreign key in tasks to project, and updated endpoints as well as created new proejct endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
-from database import get_db, init_db, Task as TaskModel
+from database import get_db, init_db, Task as TaskModel, Project as ProjectModel
 from sqlalchemy.orm import Session
 
 #initializes database
 init_db()
 
 app = FastAPI()
-tasks_db = {0: {"id": 0, "title": "homework", "done": False}}
-next_id = 1
 
 class CreateTask(BaseModel):
     title: str = Field(min_length=1, max_length=200)
     done: bool = False
+    project_id: int
 
 class UpdateTask(BaseModel):
     title: str = Field(min_length=1, max_length=200)
@@ -22,20 +21,34 @@ class TaskOut(BaseModel):
     id: int 
     title: str
     done: bool
+    project_id: int
+
+    class Config:
+        from_attributes = True
+
+class ProjectCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=200)
+
+class ProjectOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+            from_attributes = True
 
 
 @app.get("/")
 def root(): 
     return {"message": "Healthy!"}
 
-@app.get("/tasks")
+@app.get("/tasks", response_model=list[TaskOut])
 def get_all_tasks(db: Session = Depends(get_db)):
     tasks = db.query(TaskModel).all()
     return tasks 
 
 @app.post("/tasks")
 def create_task(task: CreateTask, db: Session = Depends(get_db)):
-    new_task = TaskModel(title=task.title, done=task.done)
+    new_task = TaskModel(title=task.title, done=task.done, project_id=task.project_id)
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
@@ -58,7 +71,7 @@ def edit_task(id: int, task: UpdateTask, db: Session = Depends(get_db)):
     searched_task.done = task.done
 
     db.commit()
-    db.refresh()
+    db.refresh(searched_task)
     return searched_task
     
 
@@ -70,3 +83,28 @@ def delete_task(id: int, db: Session = Depends(get_db)):
     db.delete(searched_task)
     db.commit()
     return {"message": "Deleted Task"}
+
+
+# project endpoints
+@app.get("/projects", response_model=list[ProjectOut])
+def get_projects(db: Session = Depends(get_db)):
+    all_projects = db.query(ProjectModel).all()
+    return all_projects
+
+
+@app.post("/projects", response_model=ProjectOut)
+def create_project(project: ProjectCreate, db: Session = Depends(get_db)):
+    new_project= ProjectModel(name=project.name)
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+    return new_project
+
+@app.get("/projects/{id}/tasks", response_model=list[TaskOut])
+def get_specific_project_tasks(id: int, db: Session = Depends(get_db)):
+    project = db.query(ProjectModel).filter(ProjectModel.id == id).first()
+    if project is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    specific_project_tasks = db.query(TaskModel).filter(TaskModel.project_id == id).all()
+    return specific_project_tasks
+
